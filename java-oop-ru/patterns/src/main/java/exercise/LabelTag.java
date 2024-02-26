1df6bb45c8bc62b0ecb07eee29669219880db005
@hexlet/cli <support@hexlet.io>
2024-02-26 11:36:23 +0300

    submit java-oop-ru/patterns

---
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -0,0 +1,23 @@
+package exercise;
+
+public class LabelTag implements TagInterface {
+    private String text;
+    private TagInterface childTag;
+
+    public LabelTag(String text, TagInterface childTag) {
+        this.text = text;
+        this.childTag = childTag;
+    }
+
+    @Override
+    public String render() {
+        return "<label>" + text + childTag.render() + "</label>";
+    }
+
+    public static void main(String[] args) {
+        TagInterface inputTag = new InputTag("submit", "Save");
+        TagInterface labelTag = new LabelTag("Press Submit", inputTag);
+        System.out.println(labelTag.render());
+     }
+}
+
