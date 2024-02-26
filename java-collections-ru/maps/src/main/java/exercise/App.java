2fc2625af7d6685fa8f02458424e56c9a25d22e6
@hexlet/cli <support@hexlet.io>
2024-02-26 17:15:31 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -7,13 +7,13 @@ public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> wordCount = new HashMap<>();
         String[] words = sentence.split(" ");
-        
+
         for (String word : words) {
             if (!word.isEmpty()) {
                 wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
             }
         }
-        
+
         return wordCount;
     }
 
