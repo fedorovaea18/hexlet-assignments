98b4b0d60d0b91954d37f520cd11ed9c40eaab33
@hexlet/cli <support@hexlet.io>
2024-02-27 23:17:38 +0300

    submit java-oop-ru/code-generation

--- java-oop-ru/code-generation/src/main/java/exercise/Car.java
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -9,12 +9,22 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
+@Getter
 class Car {
     int id;
     String brand;
     String model;
     String color;
     User owner;
+
+    public Car(int id, String brand, String model, String color, User owner) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.owner = owner;
+    }
     
  public String serialize() {
         ObjectMapper objectMapper = new ObjectMapper();
