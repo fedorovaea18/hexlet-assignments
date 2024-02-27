2f5e3ce6961a9a777d502e04e7789af4ba113ea2
@hexlet/cli <support@hexlet.io>
2024-02-27 19:25:28 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import org.junit.Test;
-
+import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+     
 public class Validator {
     public static List<String> validate(Address address) {
         List<String> notValidFields = new ArrayList<>();
@@ -24,6 +26,43 @@ public class Validator {
         }
         
         return notValidFields;
+    }
+        public static Map<String, List<String>> advancedValidate(Object obj) {
+        Map<String, List<String>> errors = new HashMap<>();
+        
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            
+            // Проверка на аннотацию @MinLength
+            if (field.isAnnotationPresent(MinLength.class)) {
+                MinLength annotation = field.getAnnotation(MinLength.class);
+                try {
+                    String value = (String) field.get(obj);
+                    if (value != null && value.length() < annotation.minLength()) {
+                        errors.computeIfAbsent(field.getName(), k -> new ArrayList<>())
+                              .add("Length of " + field.getName() + " is less than " + annotation.minLength());
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            // Проверка на аннотацию @NotNull
+            if (field.isAnnotationPresent(NotNull.class)) {
+                try {
+                    Object value = field.get(obj);
+                    if (value == null) {
+                        errors.computeIfAbsent(field.getName(), k -> new ArrayList<>())
+                              .add(field.getName() + " must not be null");
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        return errors;
     }
     
     @Test
