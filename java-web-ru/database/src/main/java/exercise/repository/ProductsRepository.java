668c16bd54250f96226daffc2bfe2f1fd92eb4cc
@hexlet/cli <support@hexlet.io>
2024-04-11 16:18:33 +0300

    submit java-web-ru/database

--- java-web-ru/database/src/main/java/exercise/repository/ProductsRepository.java
+++ java-web-ru/database/src/main/java/exercise/repository/ProductsRepository.java
@@ -15,9 +15,11 @@ public class ProductsRepository extends BaseRepository {
     public static void save(Product product) throws SQLException {
         String sql = "INSERT INTO products (title, price) VALUES (?, ?)";
         try (var conn = dataSource.getConnection();
-                var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+             var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
             preparedStatement.setString(1, product.getTitle());
             preparedStatement.setInt(2, product.getPrice());
+
             preparedStatement.executeUpdate();
             var generatedKeys = preparedStatement.getGeneratedKeys();
             // Устанавливаем ID в сохраненную сущность
@@ -30,15 +32,15 @@ public class ProductsRepository extends BaseRepository {
     }
 
     public static Optional<Product> find(Long id) throws SQLException {
-        var sql = "SELECT * FROM produtcs WHERE id = ?";
+        var sql = "SELECT * FROM products WHERE id = ?";
         try (var conn = dataSource.getConnection();
-                var stmt = conn.prepareStatement(sql)) {
+             var stmt = conn.prepareStatement(sql)) {
             stmt.setLong(1, id);
             var resultSet = stmt.executeQuery();
             if (resultSet.next()) {
                 var title = resultSet.getString("title");
                 var price = resultSet.getInt("price");
-                
+
                 var product = new Product(title, price);
                 product.setId(id);
                 return Optional.of(product);
@@ -50,21 +52,23 @@ public class ProductsRepository extends BaseRepository {
     public static List<Product> getEntities() throws SQLException {
         var sql = "SELECT * FROM products";
         try (var conn = dataSource.getConnection();
-                var stmt = conn.prepareStatement(sql)) {
+             var stmt = conn.prepareStatement(sql)) {
             var resultSet = stmt.executeQuery();
             var result = new ArrayList<Product>();
-            
+
             while (resultSet.next()) {
                 var id = resultSet.getLong("id");
                 var title = resultSet.getString("title");
                 var price = resultSet.getInt("price");
+
+
                 var product = new Product(title, price);
                 product.setId(id);
                 result.add(product);
             }
             return result;
         }
-    }    
+    }
     // END
 }
 
