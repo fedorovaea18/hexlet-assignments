1bea23b88be7d7d731513d7ce1551c3cbf83bb3f
@hexlet/cli <support@hexlet.io>
2024-06-04 16:14:29 +0300

    submit java-spring-ru/authentication

--- java-spring-ru/authentication/src/main/java/exercise/config/SecurityConfig.java
+++ java-spring-ru/authentication/src/main/java/exercise/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,7 +23,6 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 // BEGIN
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
     private JwtDecoder jwtDecoder;
